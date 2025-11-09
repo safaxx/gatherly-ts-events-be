37027f25aa0b26c9e71fb69ccc65d36fe84cc1b4
nooruto <noor@innov8tif.com>
2025-11-09 20:56:03 +0800

    feat: #5 - added function to generate otp

--- src/main/java/com/techsisters/gatherly/util/UserUtil.java
+++ src/main/java/com/techsisters/gatherly/util/UserUtil.java
@@ -1,11 +1,16 @@
 package com.techsisters.gatherly.util;
 
+import java.security.SecureRandom;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class UserUtil {
+
+    // Create a single, reusable instance of SecureRandom
+    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
 
     public static String ANNONYMOUS = "annonymous";
 
@@ -20,6 +25,17 @@ public class UserUtil {
 
     public static boolean isAnnonymous() {
         return StringUtils.equals(ANNONYMOUS, getLoginUsername());
+    }
+
+    /**
+     * Generates a 6-digit (000000-999999) verification code.
+     * 
+     * @return A 6-digit string code.
+     */
+    public static int generate6DigitCode() {
+        // Generate a random integer from 0 to 999,999
+        int number = SECURE_RANDOM.nextInt(1_000_000);
+        return number;
     }
 
 }
